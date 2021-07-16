b5c6a3067a6c7af2abafa8607e3652dd3c7608d0
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-07-16 18:47:39 +0100

    install font and set basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import "../styles/App.css";
 
 function App() {
-  return <div className="App">Tomogotchi</div>;
+  return (
+    <div className="App">
+      <h1>Tomogotchi</h1>
+    </div>
+  );
 }
 
 export default App;
