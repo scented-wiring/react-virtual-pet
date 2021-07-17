110ee8d2b385f4ab5e340e6d1c76b624950d7247
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-07-17 18:40:56 +0100

    cap health/hunger at 4

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -3,8 +3,8 @@ import "../styles/Actions.css";
 const Actions = ({ health, hunger, setHealth, setHunger }) => {
   return (
     <div class="Actions">
-      <button onClick={() => setHealth(health + 1)}>Play</button>
-      <button onClick={() => setHunger(hunger + 1)}> Feed</button>
+      <button onClick={() => health <= 3 && setHealth(health + 1)}>Play</button>
+      <button onClick={() => hunger <= 3 && setHunger(hunger + 1)}>Feed</button>
       <button>Clean</button>
     </div>
   );
