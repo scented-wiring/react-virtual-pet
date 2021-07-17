e29b378f94d9954da5f9db73fb8a8dfd9b844174
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-07-17 18:37:07 +0100

    add feed method to correct button (woops)

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -4,8 +4,8 @@ const Actions = ({ health, hunger, setHealth, setHunger }) => {
   return (
     <div class="Actions">
       <button onClick={() => setHealth(health + 1)}>Play</button>
-      <button>Feed</button>
-      <button onClick={() => setHunger(hunger + 1)}>Clean</button>
+      <button onClick={() => setHunger(hunger + 1)}> Feed</button>
+      <button>Clean</button>
     </div>
   );
 };
