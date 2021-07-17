eb7850525ade4a5c691278221b4c19009aeda614
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-07-17 20:17:22 +0100

    render empty icons & change burger to lemon cuz it's free

--- src/components/Status.js
+++ src/components/Status.js
@@ -8,11 +8,17 @@ const Status = ({ health, hunger }) => {
         {[...Array(health)].map((heart) => {
           return <i className="fas fa-heart"></i>;
         })}
+        {[...Array(4 - health)].map((heart) => {
+          return <i className="far fa-heart"></i>;
+        })}
       </div>
       <div class="hunger">
         Hunger:
         {[...Array(hunger)].map((burger) => {
-          return <i className="fas fa-hamburger"></i>;
+          return <i class="fas fa-lemon"></i>;
+        })}
+        {[...Array(hunger)].map((burger) => {
+          return <i class="far fa-lemon"></i>;
         })}
       </div>
     </div>
