ed8b87f6d8d67564d6fe6bb6de36a5fb83a8c510
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-07-18 11:07:02 +0100

    disable play/feed buttons if health/hunger over 3

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -3,8 +3,18 @@ import "../styles/Actions.css";
 const Actions = ({ health, hunger, setHealth, setHunger }) => {
   return (
     <div className="Actions">
-      <button onClick={() => health <= 3 && setHealth(health + 1)}>Play</button>
-      <button onClick={() => hunger <= 3 && setHunger(hunger + 1)}>Feed</button>
+      <button
+        disabled={health <= 3 ? false : true}
+        onClick={() => setHealth(health + 1)}
+      >
+        Play
+      </button>
+      <button
+        disabled={hunger <= 3 ? false : true}
+        onClick={() => setHunger(hunger + 1)}
+      >
+        Feed
+      </button>
       <button>Clean</button>
     </div>
   );
